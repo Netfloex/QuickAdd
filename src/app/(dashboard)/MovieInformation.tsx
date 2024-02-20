a3a781583ec380eaa205ddd5adfc80b70db08aa9
Netfloex <38650595+Netfloex@users.noreply.github.com>
2024-02-20 21:14:39 +0100

    Remove unnecessary dividers

--- src/app/(dashboard)/MovieInformation.tsx
+++ src/app/(dashboard)/MovieInformation.tsx
@@ -54,8 +54,12 @@ export const MovieInformation: FC<{ movie: MovieSearchResult }> = ({
 						<p>{movie.year}</p>
 						<Divider orientation="vertical" />
 						<p>{humanizeDuration(movie.runtime * 60)}</p>
-						<Divider orientation="vertical" />
-						<p>{movie.studio}</p>
+						{movie.studio && (
+							<>
+								<Divider orientation="vertical" />
+								<p>{movie.studio}</p>
+							</>
+						)}
 					</div>
 
 					{/* Ratings */}
--- src/app/(dashboard)/MovieRatings.tsx
+++ src/app/(dashboard)/MovieRatings.tsx
@@ -1,6 +1,6 @@
 import { Divider } from "@nextui-org/divider"
 import Image from "next/image"
-import { Fragment } from "react"
+import { Fragment, useMemo } from "react"
 import { FaImdb } from "react-icons/fa"
 import { SiRottentomatoes } from "react-icons/si"
 
@@ -9,66 +9,67 @@ import { MovieSearchResult } from "@schemas/MovieSearchResult"
 import type { FC } from "react"
 
 export const MovieRatings: FC<{ movie: MovieSearchResult }> = ({ movie }) => {
-	const data = [
-		{
-			icon: <FaImdb color="#f1c117" />,
-			data: movie.movieRatings.imdb,
-			percentage: false,
-			name: "IMDB",
-		},
-		{
-			icon: <SiRottentomatoes color="#f93109" />,
-			data: movie.movieRatings.rottenTomatoes,
-			percentage: true,
-			name: "Rotten Tomatoes",
-		},
-		{
-			icon: (
-				<Image
-					src="https://www.metacritic.com/favicon.ico"
-					width="16"
-					height="16"
-					unoptimized
-					alt="M"
-				/>
+	const data = useMemo(
+		() =>
+			[
+				{
+					icon: <FaImdb color="#f1c117" />,
+					data: movie.movieRatings.imdb,
+					percentage: false,
+					name: "IMDB",
+				},
+				{
+					icon: <SiRottentomatoes color="#f93109" />,
+					data: movie.movieRatings.rottenTomatoes,
+					percentage: true,
+					name: "Rotten Tomatoes",
+				},
+				{
+					icon: (
+						<Image
+							src="https://www.metacritic.com/favicon.ico"
+							width="16"
+							height="16"
+							unoptimized
+							alt="M"
+						/>
+					),
+					data: movie.movieRatings.metacritic,
+					percentage: true,
+					name: "Metacritic",
+				},
+				{
+					icon: (
+						<Image
+							src="https://www.themoviedb.org/favicon.ico"
+							width="16"
+							height="16"
+							unoptimized
+							alt="M"
+						/>
+					),
+					data: movie.movieRatings.tmdb,
+					percentage: false,
+					name: "TMDB",
+				},
+			].filter(
+				(d) =>
+					d.data !== null &&
+					!(d.name == "TMDB" && d.data.value === 0),
 			),
-			data: movie.movieRatings.metacritic,
-			percentage: true,
-			name: "Metacritic",
-		},
-		{
-			icon: (
-				<Image
-					src="https://www.themoviedb.org/favicon.ico"
-					width="16"
-					height="16"
-					unoptimized
-					alt="M"
-				/>
-			),
-			data: movie.movieRatings.tmdb,
-			percentage: false,
-			name: "TMDB",
-		},
-	]
+		[movie.movieRatings],
+	)
 
 	return (
 		<div className="flex space-x-4 h-6">
 			{data.map((d, i) => {
-				if (
-					d.data === null ||
-					(d.name === "TMDB" && d.data.value === 0)
-				) {
-					return null
-				}
-
 				return (
 					<Fragment key={d.name}>
 						<div title={d.name} className="flex gap-1 items-center">
 							{d.icon}{" "}
 							{d.percentage
-								? `${d.data.value}%`
-								: d.data.value.toFixed(1)}
+								? `${d.data!.value}%`
+								: d.data!.value.toFixed(1)}
 						</div>
 						{i != data.length - 1 && (
 							<Divider orientation="vertical" />
