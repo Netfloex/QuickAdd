332ec805612cad8b6425dad6a291e24c7246d38a
Netfloex <38650595+Netfloex@users.noreply.github.com>
2024-07-11 21:32:12 +0200

    Add RottenTomatoes link to `MovieLinks`, make `DropDownItem`s a link

--- src/app/(dashboard)/MovieLinks.tsx
+++ src/app/(dashboard)/MovieLinks.tsx
@@ -10,6 +10,7 @@ import { Link } from "@nextui-org/link"
 import Image from "next/image"
 import { FaImdb, FaYoutube } from "react-icons/fa"
 import { MdMoreVert } from "react-icons/md"
+import { SiRottentomatoes } from "react-icons/si"
 
 import { MovieSearchResult } from "@schemas/MovieSearchResult"
 
@@ -25,12 +26,12 @@ export const MovieLinks: FC<{ movie: MovieSearchResult }> = ({ movie }) => {
 			</DropdownTrigger>
 			<DropdownMenu>
 				<DropdownSection title="Links" aria-label="Links">
-					<DropdownItem startContent={<FaImdb color="#f1c117" />}>
-						<Link
-							href={`https://imdb.com/title/${movie.imdbId}`}
-							isExternal
-							showAnchorIcon
-						>
+					<DropdownItem
+						startContent={<FaImdb color="#f1c117" />}
+						href={`https://imdb.com/title/${movie.imdbId}`}
+						target="_blank"
+					>
+						<Link isExternal showAnchorIcon>
 							IMDB
 						</Link>
 					</DropdownItem>
@@ -44,22 +45,30 @@ export const MovieLinks: FC<{ movie: MovieSearchResult }> = ({ movie }) => {
 								alt="M"
 							/>
 						}
+						href={`https://themoviedb.org/movie/${movie.tmdbId}`}
+						target="_blank"
 					>
-						<Link
-							href={`https://themoviedb.org/movie/${movie.tmdbId}`}
-							isExternal
-							showAnchorIcon
-						>
+						<Link isExternal showAnchorIcon>
 							TMDB
 						</Link>
 					</DropdownItem>
-					<DropdownItem startContent={<FaYoutube color="#ff0000" />}>
-						<Link
-							href={`https://www.youtube.com/watch?v=${movie.youtubeTrailerId}`}
-							isExternal
-							showAnchorIcon
-						>
+					<DropdownItem
+						startContent={<FaYoutube color="#ff0000" />}
+						href={`https://www.youtube.com/watch?v=${movie.youtubeTrailerId}`}
+						target="_blank"
+					>
+						<Link isExternal showAnchorIcon>
 							YouTube
+						</Link>
+					</DropdownItem>
+					<DropdownItem
+						startContent={<SiRottentomatoes color="#f93109" />}
+						href={`https://duckduckgo.com/?q=\\site:www.rottentomatoes.com+${movie.title}+${movie.year}`}
+						rel="noopener noreferrer"
+						target="_blank"
+					>
+						<Link isExternal showAnchorIcon>
+							Rotten Tomatoes
 						</Link>
 					</DropdownItem>
 				</DropdownSection>
